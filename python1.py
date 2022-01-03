da2df95a1f3c2eccdc2ba96da7745b06282174fb
dungipravalikareddy <89082767+dungipravalikareddy@users.noreply.github.com>
2022-01-03 11:13:59 +0530

    third

--- python1.py
+++ python1.py
@@ -6,3 +6,13 @@ print("the reverse list is ", List1)
 List2 = [6 ,8 ,4 ,1 ,9 ,5]
 List2.sort()
 print("the sorted list is ", List2)
+
+x = 5
+y = 10
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
+
